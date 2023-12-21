5398ae45742ad035a3365a18197a0a77cb595502
arturlicks <arturlicks@gmail.com>
2023-12-21 16:46:33 -0300

    Generate components scaffold

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SmallCardComponent } from "./components/small-card/small-card.component";
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, SmallCardComponent, MenuTitleComponent]
 })
 export class AppComponent {
   title = 'angular-blog';
